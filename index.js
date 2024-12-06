b0d5c889926cccaefe4b7020a1e70771dbb3945d
jlburks <jaren.burks1@gmail.com>
2024-12-05 20:10:22 -0600

    2 icons added on map

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-
-// Google Maps Platform API Key == AIzaSyC0y3R0OeeF5VCnrpR4ZJHXMOMrxRjaqmo
-// AIzaSyC8COTRJVJkYy8cL4doW41ymJh94lw8RoY
 /**
  * @license
  * Copyright 2019 Google LLC. All Rights Reserved.
@@ -9,26 +6,32 @@
 // [START maps_add_map]
 // Initialize and add the map
 
- const golfballImg = document.createElement("img");
-  golfballImg.src =  "./img/redball.jpg"
+const golfballImg = document.createElement("img");
+golfballImg.src =  "./img/blueball.jpg"
+golfballImg.width= "20"
+golfballImg.height="20"
 
+const holeImg = document.createElement("img");
+holeImg.src =  "./img/blackflag.jpg"
+holeImg.width= "20"
+holeImg.height="20"
 
 let map;
 
 async function initMap() {
   // [START maps_add_map_instantiate_map]
   // The location of Uluru
-  const position = { lat: 29.588966244829354, lng: -95.36162478474299 };
+  const position1 = {lat:29.58896624482934, lng:-95.3619};
+  const position2 = { lat: 29.58896624482934, lng: -95.36162478474299 };
   // Request needed libraries.
   //@ts-ignore
   const { Map } = await google.maps.importLibrary("maps");
   const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
 
   // The map, centered at Uluru
-  map = new Map(document.getElementById("map"), {
-    content: golfballImg,
+  const map = new Map(document.getElementById("map"), {
     zoom: 19,
-    center: position,
+    center: position1,
     mapId: "DEMO_MAP_ID",
     mapTypeId: 'satellite',
   });
@@ -36,13 +39,21 @@ async function initMap() {
   // [END maps_add_map_instantiate_map]
   // [START maps_add_map_instantiate_marker]
   // The marker, positioned at Uluru
-  const marker = new AdvancedMarkerElement({
+  const playerIcon = new AdvancedMarkerElement({
     map: map,
-    position: position,
-    content: golfballImg
+    position: position1,
+    content: holeImg
   });
   // [END maps_add_map_instantiate_marker]
+  const holeIcon = new AdvancedMarkerElement({
+    map: map,
+    position: position2,
+    content: golfballImg
+  });
 }
 
 initMap();
 // [END maps_add_map]
+
+
+
